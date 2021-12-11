07445aed4a8ac4df0c30a841ef9abca16bd74b57
krskovape <krskovape@natur.cuni.cz>
2021-12-11 11:30:44 +0100

    Načítání souborů pomocí funkce, vyřešení Issue 2

--- du3.py
+++ du3.py
@@ -1,74 +1,62 @@
-import json
-from json.decoder import JSONDecodeError
-from funkce import prevod_souradnic, vypocet_vzdalenosti
+from funkce import prevod_souradnic, vypocet_vzdalenosti, nacteni_souboru
 
-try:
-    with open("adresy.geojson", encoding="utf-8") as a,\
-        open("kontejnery.geojson", encoding="utf-8") as k:
+#načtení vstupních souborů
+adresy = nacteni_souboru("adresy.geojson")
+kontejnery = nacteni_souboru("kontejnery.geojson")
 
-        adresy = json.load(a)
-        kontejnery = json.load(k)
+#výběr jen veřejných kontejnerů
+verejne_kontejnery = []
+counter_kontejnery = 0
+for feature in kontejnery["features"]:
+    if feature["properties"]["PRISTUP"] == "volně":
+        verejne_kontejnery.append(feature)
+        counter_kontejnery += 1
 
-        #výběr jen veřejných kontejnerů
-        verejne_kontejnery = []
-        counter_kontejnery = 0
-        for feature in kontejnery["features"]:
-            if feature["properties"]["PRISTUP"] == "volně":
-                verejne_kontejnery.append(feature)
-                counter_kontejnery += 1
+#hledání nejbližšího kontejneru
+min_vzdalenost = float('inf')
+max_vzdalenost = 0
+sum_vzdalenost = 0
+counter_adresy = 0
 
-        #hledání nejbližšího kontejneru
-        min_vzdalenost = float('inf')
-        max_vzdalenost = 0
-        sum_vzdalenost = 0
-        counter_adresy = 0
+for feature in adresy['features']:
+    #převedení souřadnic adresních bodů do S-JTSK
+    try:
+        feature["geometry"]["coordinates_sjtsk"] = list(prevod_souradnic(*feature["geometry"]["coordinates"]))
+        x1,y1 = feature["geometry"]["coordinates_sjtsk"]
+        id_adresa = feature["properties"]["@id"]
+    except ValueError:
+        print(f"U adresního bodu {id_adresa} jsou chybně zadané souřadnice a program ho přeskočí.") 
+        continue
 
-        for feature in adresy['features']:
-            #převedení souřadnic adresních bodů do S-JTSK
-            try:
-                feature["geometry"]["coordinates_sjtsk"] = list(prevod_souradnic(*feature["geometry"]["coordinates"]))
-                x1,y1 = feature["geometry"]["coordinates_sjtsk"]
-                id_adresa = feature["properties"]["@id"]
-            except ValueError:
-                print(f"U adresního bodu {id_adresa} jsou chybně zadané souřadnice a program ho přeskočí.") 
-                continue
+    #výpočet minimální vzdálenosti
+    for kontejner in verejne_kontejnery:
+        try:
+            x2,y2 = kontejner["geometry"]["coordinates"]
+            id_kontejner = kontejner["properties"]["ID"]
+        except ValueError:
+            print(f"U kontejneru {id_kontejner} jsou chybně zadané souřadnice a program ho přeskočí.")
+            continue
 
-            #výpočet minimální vzdálenosti
-            for kontejner in verejne_kontejnery:
-                try:
-                    x2,y2 = kontejner["geometry"]["coordinates"]
-                    id_kontejner = kontejner["properties"]["ID"]
-                except ValueError:
-                    print(f"U kontejneru {id_kontejner} jsou chybně zadané souřadnice a program ho přeskočí.")
-                    continue
+        vzdalenost = vypocet_vzdalenosti(x1,y1,x2,y2)
+        if vzdalenost < min_vzdalenost:
+            min_vzdalenost = vzdalenost
 
-                vzdalenost = vypocet_vzdalenosti(x1,y1,x2,y2)
-                if vzdalenost < min_vzdalenost:
-                    min_vzdalenost = vzdalenost
+    #kontrola, že minimální vzdálenost není větší než 10 km
+    if min_vzdalenost > 10000:
+        print(f"U adresního bodu {id_adresa} je vzdálenost k nejbližšímu kontejneru větší než 10 km.")
+        quit()
+    
+    if min_vzdalenost > max_vzdalenost:
+        max_vzdalenost = min_vzdalenost
+        ulice = feature["properties"]["addr:place"]
+        cislo_pop = feature["properties"]["addr:housenumber"]
 
-            #kontrola, že minimální vzdálenost není větší než 10 km
-            if min_vzdalenost > 10000:
-                print(f"U adresního bodu {id_adresa} je vzdálenost k nejbližšímu kontejneru větší než 10 km.")
-                quit()
-            
-            if min_vzdalenost > max_vzdalenost:
-                max_vzdalenost = min_vzdalenost
-                ulice = feature["properties"]["addr:place"]
-                cislo_pop = feature["properties"]["addr:housenumber"]
+    sum_vzdalenost += min_vzdalenost
+    counter_adresy += 1
 
-            sum_vzdalenost += min_vzdalenost
-            counter_adresy += 1
-        
-        prumerna_vzdalenost = int(sum_vzdalenost / counter_adresy)
+prumerna_vzdalenost = int(sum_vzdalenost / counter_adresy)
 
-        print(f"Načteno {counter_adresy} adresních bodů.")
-        print(f"Načteno {counter_kontejnery} veřejných kontejnerů.\n")
-        print(f"Průměrná vzdálenost ke kontejneru je {prumerna_vzdalenost} m.")
-        print(f"K nejbližšímu kontejneru je to nejdále z adresy {ulice} {cislo_pop} a to {max_vzdalenost} m.\n")
-
-except FileNotFoundError:
-    print("Zadaný soubor se vstupními daty nelze otevřít. Soubor neexistuje, nebo je chybně zadaná cesta k jeho umístění.")
-except PermissionError:
-    print("Program nemá dostatečná oprávnění ke vstupnímu nebo výstupnímu programu.")
-except JSONDecodeError:
-    print("Vstupní soubor není platný JSON.")
+print(f"Načteno {counter_adresy} adresních bodů.")
+print(f"Načteno {counter_kontejnery} veřejných kontejnerů.\n")
+print(f"Průměrná vzdálenost ke kontejneru je {prumerna_vzdalenost} m.")
+print(f"K nejbližšímu kontejneru je to nejdále z adresy {ulice} {cislo_pop} a to {max_vzdalenost} m.\n")
--- funkce.py
+++ funkce.py
@@ -1,5 +1,7 @@
 from pyproj import Transformer
 from math import sqrt
+import json
+from json.decoder import JSONDecodeError
 
 def prevod_souradnic(x,y):
     wgs2jtsk = Transformer.from_crs(4326,5514,always_xy=True)
@@ -7,5 +9,20 @@ def prevod_souradnic(x,y):
 
 def vypocet_vzdalenosti(x1,y1,x2,y2):
     return int(sqrt((x2-x1)**2 + (y2-y1)**2))
+
+def nacteni_souboru(nazev_souboru):
+    try:
+        with open(nazev_souboru, encoding="utf-8") as s:
+            return json.load(s)
+    except FileNotFoundError:
+        print(f"Zadaný soubor {nazev_souboru} otevřít. Soubor neexistuje, nebo je chybně zadaná cesta k jeho umístění.")
+        quit()
+    except PermissionError:
+        print(f"Program nemá dostatečná oprávnění k souboru {nazev_souboru}.")
+        quit()
+    except JSONDecodeError:
+        print(f"Vstupní soubor {nazev_souboru} není platný JSON.")
+        quit()
+
 
 
